5120c0a67e027721637fa7d6ec4276a7447261d3
Vitaly A <crazyyy@gmail.com>
2015-02-11 22:45:16 +0200

    before drmichaels.com main page

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@ var gulp = require('gulp');
 var uncss = require('gulp-uncss');
 
 gulp.task('default', function() {
-    return gulp.src('index.css')
+    return gulp.src('main.css')
         .pipe(uncss({
-            html: ['http://www.drmichaels.com/', 'http://www.drmichaels.com/contacts/dental-clinic-jumeirah-dubai']
+            html: 'index.html'
         }))
         .pipe(gulp.dest('./out'));
 });
