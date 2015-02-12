77b4a15adf5770a521be5518bfbe2241e1371d22
Vitaly A <crazyyy@gmail.com>
2015-02-12 23:14:02 +0200

    dentall inner all

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@ var gulp = require('gulp');
 var uncss = require('gulp-uncss');
 
 gulp.task('default', function() {
-    return gulp.src('main.css')
+    return gulp.src('fonts.css')
         .pipe(uncss({
-            html: 'index.html'
+            html: ['about.html', 'contact.html', 'contact-form.html', 'dental-all.html', 'dental-spec.html', 'index.html', 'index-new.html', 'services.html', 'services2.html']
         }))
         .pipe(gulp.dest('./out'));
 });
