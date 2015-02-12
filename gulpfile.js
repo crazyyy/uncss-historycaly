2ef784e62fef28f4eb4b6497dc3009be7ee49d38
Vitaly A <crazyyy@gmail.com>
2015-02-12 23:19:03 +0200

    css all

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var uncss = require('gulp-uncss');
 
 gulp.task('default', function() {
-    return gulp.src('fonts.css')
+    return gulp.src('fb9af384ac58a2ab044f3c0d022abc40.css')
         .pipe(uncss({
             html: ['about.html', 'contact.html', 'contact-form.html', 'dental-all.html', 'dental-spec.html', 'index.html', 'index-new.html', 'services.html', 'services2.html']
         }))
